0ccc6a835ed26653c6a5ea5b8d220b1987115d0c
GitBucked <desmondcampbelljr4@gmail.com>
2025-11-21 11:37:24 -0500

    Update server port to 5174 and configured CORS

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 5174;
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.json());
 
 // Cycle through models to ensure they are loaded before creating tables
 const db = require('./models');
 
+//Routers
+const postRouter = require('./Routes/Posts');
+app.use(express.json());
+app.use('/posts', postRouter);
+
 db.sequelize.sync().then(() => {
 	// API Endpoints would be defined here and where it starts listening
 		app.listen(port, () => {
-				console.log(`Server is running on http://localhost:${port}`);
+				console.log(`Server is running on http://localhost:${port}`); 
 		});
 });
