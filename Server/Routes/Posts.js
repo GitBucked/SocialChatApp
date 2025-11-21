e41ca0b6576079fb17f96e86be6269b1d7aed869
GitBucked <desmondcampbelljr4@gmail.com>
2025-11-21 11:38:34 -0500

    Add Posts route for handling GET and POST requests

---
+++ Server/Routes/Posts.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { Post } = require('../models');
+
+router.get("/", async (req, res) => {
+	const posts = await Post.findAll();
+	res.json(posts);
+});
+
+router.post("/", async (req, res) => {
+	const post = req.body;
+	await Post.create(post);
+	res.json(post);
+});
+
+module.exports = router;
