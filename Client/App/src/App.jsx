94302ae39608bd516033fdbd115df099fbf26c58
GitBucked <desmondcampbelljr4@gmail.com>
2025-11-25 14:47:03 -0500

    Add CreatePost component and CreateBtn; update routing and Home page

--- Client/App/src/App.jsx
+++ Client/App/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import Home from './pages/home';
+import CreatePost from './pages/createPost';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
 	return (
 	<div className="App">
+
 		<Router>
 			<Routes>
 				<Route path="/" element={<Home/>} />
+				<Route path="/create-post" element={<CreatePost/>} />
 			</Routes>
 		</Router>
 	</div>
---
+++ Client/App/src/pages/createPost.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreatePost from '../components/createPost'
+
+function createPost() {
+	return (
+		<div className="flex flex-col items-center min-h-screen">
+			<CreatePost />
+		</div>
+	)
+}
+
+export default createPost
