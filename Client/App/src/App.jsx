7e2c789c406be1ff5f80ca8573bfc11ccf7e0ff6
DCampbell <118453191+GitBucked@users.noreply.github.com>
2025-11-27 15:31:58 -0500

    feat: add Navigator component to App and remove unused CreateBtn from CreatePost and Home (#24)
    * Add Post page component and update routing; include dotenv dependency
    
    * fix/Dupe code in home.jsx file
    
    * Deleted dupe code in home.jsx file
    
    * Unimportant changes
    
    * fix: properly fix formatting in pages
    
    * feat: add Navigator component to App and remove unused CreateBtn from CreatePost and Home
    
    * feat: import Navigator component in App
    
    * fix: adjust formatting of Navigator component in App

--- Client/App/src/App.jsx
+++ Client/App/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import Home from './pages/home';
 import CreatePost from './pages/createPost';
 import Post from './pages/post';
+import Navigator from './components/Navigator';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navigator />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
